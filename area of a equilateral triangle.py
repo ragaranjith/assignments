bbfdf22848520bd12752b36142666d4cd549304b
ragaranjith <61176801+ragaranjith@users.noreply.github.com>
2020-03-02 15:56:54 +0530

    Add files via upload

---
+++ area of a equilateral triangle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 15:46:23 2020
+
+@author: ragar
+"""
+
+a=int(input("length of one side of equilateraltriangle:"))
+area=((3/4)**(1/2)*(a**2))
+print("area of equilateral triangle for a given side=%F" %area)
