e26e96d74c82aae9392cf2202bd6c1acdb179627
ragaranjith <61176801+ragaranjith@users.noreply.github.com>
2020-03-02 16:03:10 +0530

    Add files via upload

---
+++ finding area of triangle for given base and height.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 16:00:48 2020
+
+@author: ragar
+"""
+
+#program to find the area of the triangle when base and height are given
+b=int(input("enter the base of the triangle:"))
+h=int(input("enter the height of the triangle:"))
+area=((1/2)*b*h)
+print("area of the triangle for given base and height=%i" %area)
