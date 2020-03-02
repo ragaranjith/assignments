e6d824a33de0d0b51749bab259b21ab416411dc4
ragaranjith <61176801+ragaranjith@users.noreply.github.com>
2020-03-02 16:06:59 +0530

    Add files via upload

---
+++ power function.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 16:05:37 2020
+
+@author: ragar
+"""
+
+#program to find power of any number X^y
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+power=(x**y)
+print(power)
---
+++ square root.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 16:06:14 2020
+
+@author: ragar
+"""
+
+#program to find the square root of a number
+x=int(input("enter the number for which square root should be found:"))
+sqrt=(x**0.5)
+print("square root of given number=%i" %sqrt)
