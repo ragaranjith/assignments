5964a23366b90665a6d3b7edeac87ec70ae79d67
ragaranjith <61176801+ragaranjith@users.noreply.github.com>
2020-03-03 19:17:03 +0530

    Add files via upload

---
+++ max number with or without using conditional operator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 13:44:13 2020
+
+@author: ragar
+"""
+# with out using conditional operator
+list1=[88,99]
+max(list1)
+
+
+# with using conditional operator finding the max number btw 2 numbers
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+if x>y:
+   print(x)
+else:
+    print(y)
